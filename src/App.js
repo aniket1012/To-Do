88247a1056c46e08e41e45312c125091d78f78a7
Aniket <aniketupatel1@gmail.com>
2021-06-24 18:21:15 -0400

    added form component with select

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
+import Form from './Components/Form'
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Aniket's ToDo List</h1>
       </header>
+      <Form/>
     </div>
   );
 }
