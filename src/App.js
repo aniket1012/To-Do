d454218e49b84881870a1e84bb2a36489f666d49
Aniket <aniketupatel1@gmail.com>
2021-06-24 18:35:33 -0400

    added input text to state to record input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Form from './Components/Form'
+import TodoList from './Components/ToDoList'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Aniket's ToDo List</h1>
       </header>
       <Form/>
+      <TodoList/>
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Form = () => {
+
+   const [inputText, setInputText] =  useState('')
+    
+   const inputHandler = (event) => {
+       setInputText(event.target.value)
+       console.log(inputText);
+   }
+
     return (
         <form>
-            <input type='text' className='todo-input'/>
+            <input type='text' className='todo-input' onChange={inputHandler}/>
             <button className='todo-button' type='submit'>
-                <i className='fas fa-plus-sqare'></i>
+                <i className='fas fa-plus-square'></i>
             </button> 
             <div className='select'>
                 <select   name ='todos' className='filter-todo'>
