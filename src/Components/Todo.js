c19010c6c1c9cddb5cd8c8a783fec0b85ff3513d
Aniket <aniketupatel1@gmail.com>
2021-06-28 11:56:05 -0400

    added toggle to todo item class to show complettion

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -6,10 +6,20 @@ const Todo = (props) => {
     const deleteHandler = () => {
       props.setToDos(props.todos.filter(el => el.id !== props.todo.id))
     }
+    const completeHandler = () => {
+      props.setToDos(props.todos.map(item => {
+        if(item.id === props.todo.id) {
+          return {
+            ...item, completed: !item.completed
+          }
+        }
+        return item
+      }))
+    }
     return (
       <div className="todo">
-        <li className="todo-item">{props.todo.text}</li>
-        <button className="complete-btn">
+        <li className={`todo-item ${props.todo.completed ? "completed": ""}`}>{props.todo.text}</li>
+        <button className="complete-btn" onClick={completeHandler}>
           <i className="fas fa-check"></i>
         </button>
         <button className="trash-btn" onClick={deleteHandler}>
